874aac22e168e900655306746b3c7c9dd14f1225
fluffy_kaiju <hadadmat@gmail.com>
2022-04-05 00:06:36 +0200

    Makefile push 2022-04-05 00:06:36

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Test from '../views/Test.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
+    },
+
     {
       path: '/about',
       name: 'about',
---
+++ src/views/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="test">
+    <h1>This is an test page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .test {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+
+
+
+	//Test query
+    const urlParams = new URLSearchParams(window.location.search);
+   const name = urlParams.get("name");
+    console.log(name);
+
+
+
+
+</script>
+
+
+
