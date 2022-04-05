ec96edc170068fb4b66e8159337a034455d6a427
fluffy_kaiju <hadadmat@gmail.com>
2022-04-05 20:19:18 +0200

    Makefile push 2022-04-05 20:19:18

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="test">
-    <h1>This is an test page</h1>
+  <div id="test">
+    <h1>{{ cause }}</h1>
+	<p>{{ money }}</p>
   </div>
 </template>
 
@@ -15,13 +16,30 @@
 </style>
 <script>
 
+import axios from "axios";
 
 
 	//Test query
+	let cause;
+	let money;
     const urlParams = new URLSearchParams(window.location.search);
    const name = urlParams.get("name");
     console.log(name);
 
+	let req = await axios.get('https://streamlabscharity.com/api/v1/causes/' + name)
+	console.log(req);
+	cause = req.data.display_name;
+	money = (req.data.amount_raised / 100);
+	money += '€';	
+	export default {
+		data(){
+			return{
+				cause: cause,
+				money: money,
+			}
+		}
+	}
+		//   response => (this.test = response))
 
 
 
